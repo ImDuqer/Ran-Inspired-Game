e3e3953c4ce7a69391c49c7591734357141cac6c
Eduardo Henriques <duqer@live.com>
2021-10-24 21:03:27 -0300

    Update no design do level

--- Projeto Estudo/Assets/1 - Scripts/CameraPanning.cs
+++ Projeto Estudo/Assets/1 - Scripts/CameraPanning.cs
@@ -11,6 +11,7 @@ public class CameraPanning : MonoBehaviour
     [SerializeField] Transform CamLowPos;
     public static bool shouldPanCamera;
     bool zoom = false;
+    float panPercent;
     Vector3 pos;
     void Start() {
         shouldPanCamera = true;
@@ -19,17 +20,24 @@ public class CameraPanning : MonoBehaviour
 
     
     void Update() {
+        Debug.Log("panPercent (" + panPercent + ") = posX (" + Mathf.Abs(pos.x + 1140) + ") * 100 / 70");
         if (shouldPanCamera) {
-            if (Input.mousePosition.y >= Screen.height - panBorderThickness) pos.x -= panSpeed * Time.deltaTime;
+            if (Input.mousePosition.y >= Screen.height - panBorderThickness) {
+                pos.x -= panSpeed * Time.deltaTime;
+                CorrectHeight();
+            }
 
-            if (Input.mousePosition.y <= panBorderThickness) pos.x += panSpeed * Time.deltaTime;
+            if (Input.mousePosition.y <= panBorderThickness) {
+                pos.x += panSpeed * Time.deltaTime;
+                CorrectHeight();
+            }
 
             if (Input.mousePosition.x >= Screen.width - panBorderThickness) pos.z += panSpeed * Time.deltaTime;
 
             if (Input.mousePosition.x <= panBorderThickness) pos.z -= panSpeed * Time.deltaTime;
 
             //numeros subtraindo temporarios, referente ao centro do mapa na posição global
-            pos.x = Mathf.Clamp(pos.x, -1180 - panLimit.x, -1180 + panLimit.x);
+            pos.x = Mathf.Clamp(pos.x, -1175 - panLimit.x, -1175 + panLimit.x);
             pos.z = Mathf.Clamp(pos.z, 380 - panLimit.y, 380 + panLimit.y);
 
             transform.position = pos;
@@ -61,7 +69,12 @@ public class CameraPanning : MonoBehaviour
         zoom = !zoom;
     }
     void Zoom(Transform destination) {
-        transform.position = Vector3.Lerp(transform.position, destination.position, Time.deltaTime * 4);
-        transform.rotation = Quaternion.Lerp(transform.rotation, destination.rotation, Time.deltaTime * 4);
+        transform.SetPositionAndRotation(Vector3.Lerp(transform.position, destination.position, Time.deltaTime * 4), Quaternion.Lerp(transform.rotation, destination.rotation, Time.deltaTime * 4));
+    }
+    void CorrectHeight() {
+
+        panPercent = (Mathf.Abs(pos.x + 1140) * 100) / 70;
+        panPercent = Mathf.Clamp(panPercent, 1, 100);
+        pos.y = (panPercent * 31 / 100) + 27;
     }
 }
