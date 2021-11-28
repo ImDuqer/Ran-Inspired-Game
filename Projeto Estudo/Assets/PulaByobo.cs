6e304dc52e5d6d7bed9209571b1d18b08365e725
Eduardo Henriques <duqer@live.com>
2021-11-28 19:12:40 -0300

    Update 45
    • Byobus are skipable

--- Projeto Estudo/Assets/PulaByobo.cs
+++ Projeto Estudo/Assets/PulaByobo.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class PulaByobo : MonoBehaviour
 {
-    void Start()
-    {
-        
+    [SerializeField] GameObject circle;
+    [SerializeField] int scene = 2; 
+    float scale;
+    float maxScale;
+    bool load = false;
+
+    void Start() {
+        maxScale = circle.transform.localScale.x;
+        scale = 0;
     }
 
-    void Update()
-    {
-        if(Input.anyKeyDown){
-            SceneManager.LoadScene(2);
+    void Update() {
+        if (!load) {
+            if (Input.anyKey) {
+                scale += Time.deltaTime * 0.5f;
+                if (scale >= maxScale) {
+                    SceneManager.LoadScene(scene);
+                    load = true;
+                }
+            }
+            else {
+                if (scale > 0) scale -= Time.deltaTime;
+                else scale = 0;
+            }
+            circle.transform.localScale = new Vector3(scale, scale, 1);
         }
     }
 }
